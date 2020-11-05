8b9cbb9446caa74737ccc0d04f9e5d54b79af11d
01660046 <01660046@yto.net.cn>
2020-11-05 09:42:54 +0800

    修改

--- examples/main.js
+++ examples/main.js
@@ -2,18 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import common from './utils/common'
 import './plugins/element.js'
-import YtoModal from './../packages/index' // 导入组件
-import YtoTable from './../packages/index' // 导入组件
-import YtoRegionChoose from './../packages/index' // 导入组件
-import YtoSelectCity from './../packages/index' // 导入组件
+import yto from './../packages/index' // 导入组件
 import './styles/index.scss';
 
 Vue.config.productionTip = false
 Vue.use(common)
-Vue.use(YtoModal) // 注册组件
-Vue.use(YtoTable)
-Vue.use(YtoRegionChoose)
-Vue.use(YtoSelectCity)
+Vue.use(yto) // 注册组件
 new Vue({
   render: h => h(App),
 }).$mount('#app')
